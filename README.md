7aa42e1810395527679f3268e264a72cad059292
Joshua T Garcia <garcia.sparky@gmail.com>
2020-12-23 11:49:44 -0700

    ADDS: scaffolding, parser

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# desolate-country-data-parser
+# Desolate County - Data Parser
+Converts a CSV to a JSON for the Desolate County Project.
+
+## Running
+
+`pipenv run python src/main.py data/Name.csv`
+
+## Contributing
+
+- Install pyenv
+
+```bash
+brew install pyenv
+```
+
+- Add pyenv ENV to profile
+
+```bash
+echo "export PYENV_ROOT="$HOME/.pyenv"" >> ~/.zshrc
+echo "export PATH="$PYENV_ROOT/bin:$PATH"" >> ~/.zshrc
+```
+
+- Install pipenv
+
+```bash
+brew install pipenv
+```
+
+- Add this environment variable so pipenv uses pyenv selected version when no version is specified
+
+```bash
+echo "export PIPENV_PYTHON="$PYENV_ROOT/shims/python"" >> ~/.zshrc
+```
+
+-  Install environment
+```bash
+pipenv install --dev
+```
+
---
+++ src/main.py
@@ -0,0 +1,41 @@
+import csv
+import json
+import sys
+
+
+def _get_row_from_file(csvfile):
+    with open(csvfile) as input:
+        for row in csv.DictReader(input.read().split("\n")):
+            yield row
+        
+    
+def _get_name(csvfile):
+    name = "{}".format(csvfile.replace('data/', '').replace('.csv', ''))        
+    name_with_spaces = [' ' + char if char.isupper() else char for char in name]
+    return ''.join(name_with_spaces)
+
+
+def handle(csvfile):
+    data = []
+    for row in _get_row_from_file(csvfile):
+        clean_row = {'name': _get_name(csvfile)}
+        for key, value in row.items():
+            # make keys lowercase, remove spaces
+            key = key.lower().replace(' ', '_').replace('_(y/n)', '')
+
+            # cast lat, lng to float and yes/no into True/False
+            value = float(value) if key in ['latitude', 'longitude'] else value
+            value = True if value == 'Y' else False if value == 'N' else value  
+
+            # get rid of columns we don't need
+            if key not in ['misc', '']:
+                clean_row[key] = value
+        data.append(clean_row)
+
+    jsonfile = "{}.json".format(csvfile.rstrip('.csv'))
+    with open(jsonfile, 'w', encoding='utf-8') as output:
+        output.write(json.dumps(data, indent=4))
+
+
+if __name__ == '__main__':
+    handle(sys.argv[1])
